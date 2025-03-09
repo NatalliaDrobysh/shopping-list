4552a9fe572080184f4b4acdc706f7a870d51d61
Natallia Drobysh <natashka.drobyshh@gmail.com>
2025-03-09 21:12:28 +0300

    Домашнее задание - 4

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const sendInput = document.querySelector('#input');
+sendInput.addEventListener('keydown', function(event) {
+        if(event.key == 'Enter' && sendInput.value != "") {
+          const items = document.querySelector('.items');
+          const listElements = document.createElement('li');
+          items.append(listElements);
+          listElements.append(sendInput.value);
+          
+          sendInput.value = "";
+
+          listElements.addEventListener('click', function(){
+          listElements.classList.toggle('done');
+          })
+        }
+ 
+})
+
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
@@ -7,3 +26,4 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
